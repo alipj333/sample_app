3c1095dd8f41ad4b52a0a33ed105ce05acc42eef
Ali Jafarian <alipj333@gmail.com>
2013-02-06 21:01:29 -0700

    check-in before bed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title = "Home | Rails Tutorial"
   end
   
   def about
+  	@title = "About | Rails Tutorial"
   end
 
   def help
+  	@title = "Help | Rails Tutorial"
   end
 end
